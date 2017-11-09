94f4274ca43cf65f95cf8d0be37add9ec09fe021
Davide Frey <frey.davide.ae@gmail.com>
2017-11-09 22:45:03 +0100

    Path planner updated, added sleep functionality

--- src/path_planner_node.py
+++ src/path_planner_node.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 import rospy
+import rosbag
 import message_filters
 import space_msgs
 from geometry_msgs.msg import Vector3
 from path_optimizer import PathOptimizer
 
 from space_msgs.msg import SatelitePose
+import numpy as np
 
-optimizer = PathOptimizer()
+
+bag = rosbag.Bag('/home/dfrey/polybox/test.bag', 'w')
 
 if __name__=='__main__':
     rospy.init_node('path_planner', anonymous=True)
+
+    optimizer = PathOptimizer()
 
     pub_deltaV = rospy.Publisher('deltaV', Vector3, queue_size=10)
 
@@ -24,4 +29,44 @@ if __name__=='__main__':
     # optimizer.find_optimal_path(target_oe,chaser_oe)
     ts = message_filters.TimeSynchronizer([target_oe_sub, chaser_oe_sub], 10)
     ts.registerCallback(optimizer.callback)
-    rospy.spin()
+
+    #rospy.spin()
+
+    rate = rospy.Rate(0.1)
+    while not rospy.is_shutdown():
+        oe = optimizer.kep_chaser
+        rospy_now = rospy.Time.now()
+
+        deltaV = Vector3()
+        deltaV.x = optimizer.deltaV[0]
+        deltaV.y = optimizer.deltaV[1]
+        deltaV.z = optimizer.deltaV[2]
+        msg = SatelitePose()
+
+        msg.header.stamp = rospy_now
+        msg.header.frame_id = "teme"
+
+        msg.position.semimajoraxis = oe.a
+        msg.position.eccentricity = oe.e
+        msg.position.inclination = np.rad2deg(oe.i)
+        msg.position.arg_perigee = np.rad2deg(oe.w)
+        msg.position.raan = np.rad2deg(oe.O)
+        msg.position.true_anomaly = np.rad2deg(0.2)
+
+        msg.orientation.x = 0
+        msg.orientation.y = 0
+        msg.orientation.z = 0
+        msg.orientation.w = 0
+
+        if optimizer.sleep_flag:
+            pub_deltaV.publish(deltaV)
+            rospy.sleep(200)
+            optimizer.sleep_flag = False
+            print deltaV
+        #print msg
+        bag.write('deltaV', deltaV, rospy_now)
+        # bag.write('target_oe')
+        bag.write('chaser_oe', msg, rospy_now)
+        rate.sleep()
+
+    bag.close()
