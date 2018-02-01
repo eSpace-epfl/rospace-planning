8247ad60167e7c13e45e4c7b8e8049dad44bc03e
Davide Frey <frey.davide.ae@gmail.com>
2018-02-01 20:15:55 +0100

    Added real offline path planner

--- src/offline_path_planner.py
+++ src/offline_path_planner.py
@@ -1,11 +1,5 @@
 from scenario import Scenario
 from solver import Solver
-# from yaml import load, dump
-# try:
-#     from yaml import CLoader as Loader, CDumper as Dumper
-# except ImportError:
-#     from yaml import Loader, Dumper
-#--------------------
 
 # Import scenario and initial conditions
 scenario = Scenario()
@@ -14,3 +8,6 @@ scenario.import_yaml_scenario()
 # Solve scenario
 solver = Solver()
 solver.solve_scenario(scenario)
+
+# Save manoeuvre plan
+scenario.export_solved_scenario(solver.manoeuvre_plan)
