1898f05e024e7cf387c6dafbb28ce25b20478537
Davide Frey <frey.davide.ae@gmail.com>
2018-02-08 14:21:38 +0100

    Redefined offline path planner according to standard main method

--- src/offline_path_planner.py
+++ src/offline_path_planner.py
@@ -1,13 +1,17 @@
 from scenario import Scenario
 from solver import Solver
 
-# Import scenario and initial conditions
-scenario = Scenario()
-scenario.import_yaml_scenario()
+def main():
+    # Import scenario and initial conditions
+    scenario = Scenario()
+    scenario.import_yaml_scenario()
 
-# Solve scenario
-solver = Solver()
-solver.solve_scenario(scenario)
+    # Solve scenario
+    solver = Solver()
+    solver.solve_scenario(scenario)
 
-# Save manoeuvre plan
-scenario.export_solved_scenario(solver.manoeuvre_plan)
+    # Save manoeuvre plan
+    scenario.export_solved_scenario(solver.manoeuvre_plan)
+
+if __name__  == "__main__":
+    main()
