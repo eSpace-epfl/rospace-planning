60d760b731f11b450eac6118563294f01b7477c6
Davide Frey <frey.davide.ae@gmail.com>
2018-03-22 21:27:47 +0100

    Created new class OrbitAdjuster that will contain all the possible manoeuvres

---
+++ src/orbit_adjuster.py
@@ -0,0 +1,99 @@
+
+
+class OrbitAdjuster(object):
+
+    def __init__(self):
+        pass
+
+    def is_necessary(self):
+        pass
+
+
+class ArgumentOfPerigee(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
+
+
+class PlaneOrientation(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
+
+
+class HohmannTransfer(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
+
+
+class AnomalySynchronisation(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
+
+
+class Drift(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
+
+
+class MultiLambert(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
+
+
+class ClohessyWiltshire(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
+
+
+class TschaunerHempel(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
+
+
+class HamelDeLafontaine(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
+
+
+class GeneticAlgorithm(OrbitAdjuster):
+
+    def __init__(self):
+        pass
+
+    def evaluate_manoeuvre(self):
+        pass
