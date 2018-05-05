cadc4facbce9839ed2bd69c02cd1099e7ed85da9
Davide Frey <frey.davide.ae@gmail.com>
2018-05-05 16:07:46 +0200

    Renewed and corrected unit test

--- path_planner/test/test_orbadj.py
+++ path_planner/test/test_orbadj.py
@@ -7,9 +7,7 @@ import numpy as np
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../src/")  # hack...
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../../../rdv-cap-sim/src/")
 
-import rospace_lib
 from state import *
-from datetime import datetime
 from checkpoint import *
 from orbit_adjuster import *
 
@@ -19,27 +17,17 @@ class OrbAdjTest(unittest.TestCase):
     """
 
     def test_multi_lambert(self):
+        """
+            Test multi-lambert orbit adjuster.
+            Given initial conditions 'test_rel', check if the multi-lambert perform a correct manoeuvre in case of
+            2-body propagation.
+        """
 
-        date = datetime.utcnow()
+        target = Satellite()
+        target.initialize_satellite('test_rel', 'target', '2-body')
 
-        target = Satellite(date)
-        target.mass = 0.82
-
-        chaser = Chaser(date)
-        chaser.mass = 30.0
-
-        R_T_0 = np.array([1622.39, 5305.10, 3717.44])
-        V_T_0 = np.array([-7.29977, 0.492357, 2.48318])
-
-        target.abs_state.R = R_T_0
-        target.abs_state.V = V_T_0
-
-        chaser.rel_state.R = np.array([ -9.38182562e-01,  -9.99760428e-01,   2.92821323e-13])
-        chaser.rel_state.V = np.array([  1.16197802e-07,   1.62816739e-03,   5.55477431e-16])
-        chaser.abs_state.from_lvlh_frame(target.abs_state, chaser.rel_state)
-
-        target.prop.initialize_propagator('target', target.get_osc_oe(), '2-body')
-        chaser.prop.initialize_propagator('chaser', chaser.get_osc_oe(), '2-body')
+        chaser = Chaser()
+        chaser.initialize_satellite('test_rel', 'chaser', '2-body', target)
 
         checkpoint = RelativeCP()
         checkpoint.rel_state.R = np.array([0.0, 1.0, 0.0])
@@ -48,36 +36,24 @@ class OrbAdjTest(unittest.TestCase):
         checkpoint.t_max = 7980
 
         orb_adj = MultiLambert()
-        man = orb_adj.evaluate_manoeuvre(chaser, checkpoint, target, [0.0, 0.0, 0.0], True)
-
-        dt = (man[1].execution_epoch - man[0].execution_epoch).total_seconds()
+        orb_adj.evaluate_manoeuvre(chaser, checkpoint, target, [0.0, 0.0, 0.0], True)
 
         self.assertLess(abs(chaser.rel_state.R[0]), 1e-2)
         self.assertLess(abs(chaser.rel_state.R[1] - 1.0), 1e-2)
         self.assertLess(abs(chaser.rel_state.R[2]), 1e-2)
 
     def test_clohessy_wiltshire(self):
+        """
+            Test clohessy-wiltshire orbit adjuster.
+            Given initial conditions 'test_rel', check if the clohessy-wiltshire perform a correct manoeuvre in case of
+            2-body propagation.
+        """
 
-        date = datetime.utcnow()
+        target = Satellite()
+        target.initialize_satellite('test_rel', 'target', '2-body')
 
-        target = Satellite(date)
-        target.mass = 0.82
-
-        chaser = Chaser(date)
-        chaser.mass = 30.0
-
-        R_T_0 = np.array([1622.39, 5305.10, 3717.44])
-        V_T_0 = np.array([-7.29977, 0.492357, 2.48318])
-
-        target.abs_state.R = R_T_0
-        target.abs_state.V = V_T_0
-
-        chaser.rel_state.R = np.array([ -9.38182562e-01,  -9.99760428e-01,   2.92821323e-13])
-        chaser.rel_state.V = np.array([  1.16197802e-07,   1.62816739e-03,   5.55477431e-16])
-        chaser.abs_state.from_lvlh_frame(target.abs_state, chaser.rel_state)
-
-        target.prop.initialize_propagator('target', target.get_osc_oe(), '2-body')
-        chaser.prop.initialize_propagator('chaser', chaser.get_osc_oe(), '2-body')
+        chaser = Chaser()
+        chaser.initialize_satellite('test_rel', 'chaser', '2-body', target)
 
         checkpoint = RelativeCP()
         checkpoint.rel_state.R = np.array([0.0, 1.0, 0.0])
@@ -86,9 +62,7 @@ class OrbAdjTest(unittest.TestCase):
         checkpoint.t_max = 7980
 
         orb_adj = ClohessyWiltshire()
-        man = orb_adj.evaluate_manoeuvre(chaser, checkpoint, target)
-
-        dt = (man[1].execution_epoch - man[0].execution_epoch).total_seconds()
+        orb_adj.evaluate_manoeuvre(chaser, checkpoint, target)
 
         self.assertLess(abs(chaser.rel_state.R[0]), 1e-2)
         self.assertLess(abs(chaser.rel_state.R[1] - 1.0), 1e-2)
--- path_planner/test/test_output.py
+++ path_planner/test/test_output.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../src/")  # 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../../../rdv-cap-sim/src/")
 
 import offline_path_planner
-import rospace_lib
+
 
 class OutputTest(unittest.TestCase):
     """
@@ -17,14 +17,37 @@ class OutputTest(unittest.TestCase):
 
     def test_sample_absolute(self):
         """
-            Test output using scenario_sample_absolute.yaml
+            Test output using scenario_sample_absolute.yaml and using standard initial condition (std_ic).
+            Note that this test is performed using 2-body propagator.
 
-            Total DeltaV :
+            Check if the total deltav and the final position match the following:
+                Total DeltaV: 0.018327823164056397 km/s
+                Final position: [-5820.23998662 -1859.35110161 -3559.27141534] km
         """
 
-        dvtot = offline_path_planner.main('scenario_sample_absolute')
+        dvtot, ch_final = offline_path_planner.main('scenario_sample_absolute')
 
-        self.assertAlmostEqual(dvtot, 0.0176018439814, 5)
+        self.assertAlmostEqual(dvtot, 0.018327823164056397, 5)
+        self.assertAlmostEqual(ch_final.R[0], -5820.23998662, 2)
+        self.assertAlmostEqual(ch_final.R[1], -1859.35110161, 2)
+        self.assertAlmostEqual(ch_final.R[2], -3559.27141534, 2)
+
+    def test_sample_relative(self):
+        """
+            Test output using scenario_sample_relative.yaml and using standard initial condition (std_ic).
+            Note that this test is performed using 2-body propagator.
+
+            Check if the total deltav and the final position match the following:
+                Total DeltaV: 0.023750081806959145 km/s
+                Final position: [5622.97492523  1720.21133619  3910.33396728] km
+        """
+
+        dvtot, ch_final = offline_path_planner.main('scenario_sample_relative')
+
+        self.assertAlmostEqual(dvtot, 0.023750081806959145, 5)
+        self.assertAlmostEqual(ch_final.R[0], 5622.97492523, 2)
+        self.assertAlmostEqual(ch_final.R[1], 1720.21133619, 2)
+        self.assertAlmostEqual(ch_final.R[2], 3910.33396728, 2)
 
 
 if __name__ == '__main__':
